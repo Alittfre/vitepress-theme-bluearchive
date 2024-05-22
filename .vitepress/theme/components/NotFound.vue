2f60dc4bfba904ff4f50bfb044bb11afa24b8492
Alittfre <alittfre@gmail.com>
2024-05-22 21:19:24 +0800

    优化404页面

--- .vitepress/theme/components/NotFound.vue
+++ .vitepress/theme/components/NotFound.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="NotFound">
     <img src="../assets/NotFound.png" alt="" />
-    <a :href="base">回到主页</a>
+    <span>页面不存在</span>
+    <span class="band"><a :href="base">回到主页</a></span>
   </div>
 </template>
 <script setup lang="ts">
@@ -16,17 +17,52 @@ const base = useData().site.value.base
   align-items: center;
   width: 50vw;
   margin: 100px auto;
+  padding: 16px;
   border-radius: 32px;
   border: solid 2px white;
   backdrop-filter: var(--blur-val);
+
+
   img {
-    width: 100%;
+    width: 25vw;
+
+    &+span {
+      font-style: italic;
+      text-shadow: 0 0 5px rgba(0, 0, 0, 0.8);
+      font-size: 30px;
+      color: #fbfbfb;
+      margin-bottom: 16px;
+    }
   }
+
   a {
-    padding: 10px;
-    background-color: var(--btn-background);
-    color: var(--font-color-gold);
-    border-radius: 8px;
+    color: #74d8f9;
+    font-size: 20px;
+    font-weight: bold;
+  }
+
+}
+
+.band {
+  display: flex;
+  justify-content: center;
+  width: 30vw;
+  border-top: 2px solid #5e87b4;
+  border-bottom: 2px solid #5e87b4;
+  background-color: #1a2b51;
+  padding: 5px 0;
+  mask: linear-gradient(to left, transparent 0%, #1a2b51 50%, transparent 100%);
+}
+
+@media(max-width: 768px) {
+  .NotFound {
+    img {
+      width: 50vw;
+    }
+  }
+
+  .band {
+    width: 50vw;
   }
 }
 </style>
