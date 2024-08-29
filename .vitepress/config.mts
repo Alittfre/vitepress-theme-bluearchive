d657ddb0966d0fb327a054e9bdd328fd59800237
naahi-i <2228055359@qq.com>
2024-08-29 17:55:34 +0800

    添加welcomebox动画
    添加welcomebox动画

--- .vitepress/config.mts
+++ .vitepress/config.mts
@@ -61,7 +61,10 @@ export default defineConfigWithTheme<ThemeConfig>({
     //banner区配置
     name: "Sensei's 部落格",
     welcomeText: 'Hello, VitePress',
-    motto: '何気ない日常で、ほんの少しの奇跡を見つける物語。',
+    motto: [
+      '和你的日常，就是奇迹',
+      '何気ない日常で、ほんの少しの奇跡を見つける物語。',
+    ],
     social: [
       { icon: 'github', url: 'https://github.com/' },
       { icon: 'bilibili', url: 'https://www.bilibili.com/' },
--- .vitepress/theme/components/Welcome-box.vue
+++ .vitepress/theme/components/Welcome-box.vue
@@ -6,75 +6,113 @@
     @mouseleave="reset"
     :style="{ transform: `rotateY(${calcY}deg) rotateX(${calcX}deg)` }"
   >
-    <span class="welcome-text">{{ welcomeText }}</span>
-    <div
-      class="info-box"
-      :style="{
-        background: `linear-gradient(${angle}deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.5))`,
-      }"
-    >
-      <img src="../assets/banner/avatar.jpg" alt="" class="avatar" />
-      <span class="name">{{ name }}</span>
-      <span class="motto">{{ mottoText }}<span class="pointer"></span></span>
-      <ul>
-        <li v-for="item in social">
-          <a :href="item.url"><i :class="`iconfont icon-${item.icon} social`"></i></a>
-        </li>
-      </ul>
-    </div>
+    <transition name="fade-up" appear>
+      <span v-if="visible" class="welcome-text">{{ welcomeText }}</span>
+    </transition>
+
+    <transition name="fade-up" appear>
+      <div
+        v-if="visible"
+        class="info-box"
+        :style="{
+          background: `linear-gradient(${angle}deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.5))`,
+        }"
+      >
+        <transition name="fade-up" appear>
+          <img src="../assets/banner/avatar.jpg" alt="" class="avatar" />
+        </transition>
+
+        <transition name="fade-up" appear>
+          <span class="name">{{ name }}</span>
+        </transition>
+
+        <transition name="fade-up" appear>
+          <span class="motto">
+            {{ mottoText }}
+            <span class="pointer"></span>
+          </span>
+        </transition>
+
+        <transition-group name="fade-up" tag="ul">
+          <li v-for="item in social" :key="item.url">
+            <a :href="item.url" target="_blank" rel="noopener noreferrer">
+              <i :class="`iconfont icon-${item.icon} social`"></i>
+            </a>
+          </li>
+        </transition-group>
+      </div>
+    </transition>
   </div>
 </template>
-<script setup lang="ts">
-import { useData } from 'vitepress'
-const themeConfig = useData().theme.value
-const name = themeConfig.name
-const welcomeText = themeConfig.welcomeText
-const motto = themeConfig.motto
-const social = themeConfig.social
 
-import { ref } from 'vue'
-const multiple = 30
-const welcomeBoxRef = ref<HTMLElement | null>(null)
-let calcY = ref(0)
-let calcX = ref(0)
-let angle = ref(0)
+<script setup lang="ts">
+import { useData } from 'vitepress';
+import { ref, onMounted, nextTick } from 'vue';
+
+const themeConfig = useData().theme.value;
+const name = themeConfig.name;
+const welcomeText = themeConfig.welcomeText;
+const motto = themeConfig.motto;
+const social = themeConfig.social;
+
+const multiple = 30;
+const welcomeBoxRef = ref<HTMLElement | null>(null);
+const calcY = ref(0);
+const calcX = ref(0);
+const angle = ref(0);
+const visible = ref(false);
+
 const parallax = (e: MouseEvent) => {
   if (welcomeBoxRef.value) {
     window.requestAnimationFrame(() => {
-      let box = welcomeBoxRef.value!.getBoundingClientRect()
-      calcY.value = (e.clientX - box.x - box.width / 2) / multiple
-      calcX.value = -(e.clientY - box.y - box.height / 2) / multiple
+      const box = welcomeBoxRef.value!.getBoundingClientRect();
+      calcY.value = (e.clientX - box.x - box.width / 2) / multiple;
+      calcX.value = -(e.clientY - box.y - box.height / 2) / multiple;
       angle.value = Math.floor(
-        getMouseAngle(e.clientY - box.y - box.height / 2, e.clientX - box.x - box.width / 2),
-      )
-    })
+        getMouseAngle(
+          e.clientY - box.y - box.height / 2,
+          e.clientX - box.x - box.width / 2
+        )
+      );
+    });
   }
-}
-function getMouseAngle(x, y) {
-  const radians = Math.atan2(y, x)
-  let angle = radians * (180 / Math.PI)
+};
+
+const getMouseAngle = (x: number, y: number) => {
+  const radians = Math.atan2(y, x);
+  let angle = radians * (180 / Math.PI);
 
   if (angle < 0) {
-    angle += 360
+    angle += 360;
   }
 
-  return angle
-}
+  return angle;
+};
+
 const reset = () => {
-  calcX.value = calcY.value = angle.value = 0
-}
+  calcX.value = calcY.value = angle.value = 0;
+};
 
-let index = 0
-let mottoText = ref('')
-function addNextCharacter() {
-  if (index < motto.length) {
-    mottoText.value += motto[index]
-    index++
-    setTimeout(addNextCharacter, Math.random() * 150 + 30)
+let index = 0;
+const mottoText = ref('');
+const randomMotto = motto[Math.floor(Math.random() * motto.length)];
+
+const addNextCharacter = () => {
+  if (index < randomMotto.length) {
+    mottoText.value += randomMotto[index];
+    index++;
+    setTimeout(addNextCharacter, Math.random() * 150 + 50);
   }
-}
-addNextCharacter()
+};
+
+onMounted(() => {
+  addNextCharacter()
+  setTimeout(() => {
+    visible.value = true
+  }, 100)
+})
 </script>
+
 <style scoped lang="less">
 .welcome-box {
   display: flex;
@@ -85,6 +123,16 @@ addNextCharacter()
   z-index: 100;
   transform-style: preserve-3d;
   transition: all 0.2s;
+  perspective: 1000px;
+}
+
+.fade-up-enter-active {
+  transition: opacity 0.5s ease-out, transform 0.5s ease-out;
+}
+
+.fade-up-enter-from {
+  opacity: 0;
+  transform: translateY(30px);
 }
 
 .welcome-text {
@@ -93,21 +141,22 @@ addNextCharacter()
   color: white;
   text-shadow: 0 0 5px rgba(0, 0, 0, 0.8);
   text-align: center;
+  margin-bottom: 100px;
 }
 
 .info-box {
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
   position: relative;
-  margin-top: 80px;
-  padding-top: 50px;
-  height: 200px;
-  width: 768px;
-  border-radius: 32px;
+  padding: 60px 40px 35px 40px;
+  width: 720px;
+  border-radius: 50px;
   border: solid 2px white;
+  box-shadow: 0 8px 32px rgba(0, 0, 0, 0.2);
   backdrop-filter: var(--blur-val);
+  
+
 
   .avatar {
     position: absolute;
@@ -118,28 +167,33 @@ addNextCharacter()
     height: auto;
     border-radius: 50%;
     border: solid 3px white;
-    transition: all 0.5s;
+    margin-bottom: 20px;
+    transition: transform 0.6s ease;
+    cursor: pointer;
 
     &:hover {
-      transform: translateX(-50%) rotate(1turn);
+      transform: translateX(-50%) rotate(1turn) scale(1.1);
     }
   }
 
   .name {
-    font-weight: normal;
+    font-size: 25px;
+    margin-bottom: 28px;
+    margin-top: 24px;
   }
-
   .motto {
+    font-size: 19px;
     font-weight: bold;
     animation: colorChange 0.8s linear infinite;
     padding-right: 4px;
+    margin-bottom: 1px;
     .pointer {
       display: inline-block;
-      margin: 0 0 0 3px;
+      margin: -5px 0 0 2px;
       padding: 0;
       vertical-align: middle;
       width: 2px;
-      height: 14px;
+      height: 16px;
       animation: colorChange 0.8s linear infinite;
       background-color: var(--pointerColor);
     }
@@ -156,13 +210,6 @@ addNextCharacter()
     }
   }
 
-  span {
-    margin-top: 10px;
-    text-align: center;
-    margin-right: 16px;
-    margin-left: 16px;
-  }
-
   ul {
     display: flex;
     align-items: center;
@@ -171,6 +218,23 @@ addNextCharacter()
     width: 200px;
     padding: 0;
 
+    li {
+      opacity: 0;
+      transform: translateY(16px); /* 初始上浮位置 */
+      animation: fadeUp 0.6s forwards; /* 上浮动画 */
+      animation-delay: calc(0.1s * var(--index)); /* 自动化延迟时间 */
+      --index: 1;
+    }
+
+    li:nth-child(1) { --index: 1; }
+    li:nth-child(2) { --index: 2; }
+    li:nth-child(3) { --index: 3; }
+    li:nth-child(4) { --index: 4; }
+    li:nth-child(5) { --index: 5; }
+    li:nth-child(6) { --index: 6; }
+    li:nth-child(7) { --index: 7; }
+    /* 可以继续添加 */
+    
     .social {
       font-size: 32px;
       transition: all 0.5s;
@@ -181,16 +245,36 @@ addNextCharacter()
       }
     }
   }
+
+  @keyframes fadeUp {
+    to {
+      opacity: 1;
+      transform: translateY(0); /* 结束位置 */
+    }
+  }
 }
 
 @media (max-width: 768px) {
   .banner {
     .welcome-text {
-      font-size: 50px;
+      font-size: 45px;
+      margin-top: -20px;
     }
 
     .info-box {
-      width: 95vw;
+      width: 78vw;
+    }
+
+    .name {
+      font-size: 22px;
+    }
+
+    .motto {
+      font-size: 17px;
+    }
+
+    ul {
+      width: 180px;
     }
   }
 }
