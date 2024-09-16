d235a4a233c94b4ce9fd1b5c1bfece269d62f5e1
naahi-i <2228055359@qq.com>
2024-09-16 11:56:28 +0800

    修复自动收回菜单图标小bug

--- .vitepress/theme/components/Navbar/index.vue
+++ .vitepress/theme/components/Navbar/index.vue
@@ -13,7 +13,7 @@
       </span>
       <span class="control">
         <label class="hamburger">
-          <input type="checkbox" @change="toggleDropdownMenu" />
+          <input type="checkbox" :checked="state.showDropdownMenu" @change="toggleDropdownMenu" />
           <svg viewBox="0 0 32 32">
             <path class="line line-top-bottom"
               d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22">
