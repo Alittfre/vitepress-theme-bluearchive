817eb84e631eeb6ecbac97ed5734198ff4c93b04
naahi-i <2228055359@qq.com>
2024-09-16 14:28:35 +0800

    点击空白处可收回菜单
    - 点击空白处可收回菜单

--- .vitepress/theme/components/Navbar/Dropdown-Menu.vue
+++ .vitepress/theme/components/Navbar/Dropdown-Menu.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dropdown-menu">
+  <div class="dropdown-menu" ref="dropdownMenu">
     <div class="menu-content">
       <div class="first-row">
         <MusicControl></MusicControl>
@@ -11,9 +11,36 @@
 </template>
 
 <script setup lang="ts">
+import { ref, watch } from 'vue'
 import MusicControl from './Music-Control.vue'
 import SearchButton from './Search-Button.vue'
 import ToggleSwitch from './ToggleSwitch.vue'
+import { useStore } from '../../store'
+const { state } = useStore()
+
+const dropdownMenu = ref(null)
+// 监听点击事件并判断是否在元素内
+const handleClickOutside = (event: MouseEvent) => {
+  if (dropdownMenu.value && !dropdownMenu.value.contains(event.target as Node)) {
+    setTimeout(() => {
+      state.showDropdownMenu = false;
+    }, 1); // 避免与点击菜单控件时相冲突
+  }
+}
+
+// 监听 showDropdownMenu 的变化
+watch(
+  () => state.showDropdownMenu,
+  (newValue) => {
+    if (newValue) {
+      // showDropdownMenu 为 true 时，添加全局点击监听
+      document.addEventListener('click', handleClickOutside);
+    } else {
+      // showDropdownMenu 为 false 时，移除全局点击监听
+      document.removeEventListener('click', handleClickOutside);
+    }
+  }
+);
 </script>
 
 <style scoped lang="less">
